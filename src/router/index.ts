f2aaa37a7cee291cc9b1e629b7c5e4126e5cf614
Prog Hayder <prog.hayder.najaf@gmail.com>
2025-06-27 18:16:32 +0300

    تعديلات على هيكل المشروع

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ export const router = createRouter({
             component: HomeView
         },
         {
-            path: 'ChairManagement',
+            path: '/ChairManagement',
             name: 'ChairManagement',
             component: ChairManagementView
         }
