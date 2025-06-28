a2042fa7a04c12101139606774221b4bb1b81206
Mqnr5 <riza.aliraqy@gmail.com>
2025-06-28 04:38:09 +0300

    تعديل ملف الرواتر

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,24 @@
-import HomeView from "@/views/HomeView.vue";
-import { createRouter, createWebHistory } from "vue-router";
-import ChairManagementView from "@/views/ChairManagementView.vue";
 
-export const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'Home',
-            component: HomeView
-        },
-        {
-            path: '/ChairManagement',
-            name: 'ChairManagement',
-            component: ChairManagementView
-        },
-        {
-  path: '/ManageUsers',
-  name: 'ManageUsers',
-  component: () => import('@/components/ManageUsers.vue')
-},
-{
-  path: '/SystemSettings',
-  name: 'SystemSettings',
-  component: () => import('@/components/SystemSettings.vue')
-}
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import ChairManagementView from '@/views/ChairManagementView.vue'
 
+const ManageUsers = () => import('@/components/ManageUsers.vue')
+const SystemSettings = () => import('@/components/SystemSettings.vue')
+const SuperAdmin = () => import('@/components/SuperAdmin.vue')
 
-    ]
+const routes = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/ChairManagement', name: 'ChairManagement', component: ChairManagementView },
+  { path: '/ManageUsers', name: 'ManageUsers', component: ManageUsers },
+  { path: '/SystemSettings', name: 'SystemSettings', component: SystemSettings },
+  { path: '/SuperAdmin', name: 'SuperAdmin', component: SuperAdmin },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 })
 
-
+export default router
