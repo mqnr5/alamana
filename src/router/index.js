86570f282d4fb5ea080ec2bd09f629980cc59353
Prog Hayder <prog.hayder.najaf@gmail.com>
2025-06-29 22:12:24 +0300

    تعديل الراوتر

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-// الواجهات العامة
 import HomeView from "@/views/HomeView.vue";
 import SuperAdminView from "@/views/SuperAdminView.vue";
 import ChairManagementView from "@/views/ChairManagementView.vue";
 import ManageUsersView from "@/views/ManageUsersView.vue";
 import SystemSettingsView from "@/views/SystemSettingsView.vue";
 import DepartmentHeadView from "@/views/DepartmentHeadView.vue";
-
-// الواجهات الخاصة بالموظف
 import EmployeeView from "@/views/EmployeeView.vue";
 import EditEmployeeInfoView from "@/views/EditEmployeeInfoView.vue";
 import TasksView from "@/views/TasksView.vue";
 import PerformanceReportsView from "@/views/PerformanceReportsView.vue";
 import MajorRequestsView from "@/views/MajorRequestsView.vue";
-
-// واجهات فرعية خاصة بالموظف
-import EmployeeLeaves from "@/views/employee/EmployeeLeaves.vue";
+import EmployeeLeavesView from "@/views/EmployeeLeavesView.vue";
 
 const router = createRouter({
   history: createWebHistory('/'),
   routes: [
     { path: '/', name: 'Home', component: HomeView },
-
-    // صلاحيات إدارية
     { path: '/SuperAdmin', name: 'SuperAdmin', component: SuperAdminView },
     { path: '/ChairManagement', name: 'ChairManagement', component: ChairManagementView, props: true },
     { path: '/ManageUsers', name: 'ManageUsers', component: ManageUsersView },
     { path: '/SystemSettings', name: 'SystemSettings', component: SystemSettingsView },
     { path: '/DepartmentHead', name: 'DepartmentHead', component: DepartmentHeadView },
-
-    // الموظف
     { path: '/Employee', name: 'Employee', component: EmployeeView },
     { path: '/EditEmpInfo', name: 'EditEmpInfo', component: EditEmployeeInfoView },
     { path: '/Tasks', name: 'Tasks', component: TasksView },
     { path: '/PerformanceReports', name: 'PerformanceReports', component: PerformanceReportsView },
     { path: '/MajorRequests', name: 'MajorRequests', component: MajorRequestsView },
-
-    // 👇 واجهة الإجازات (فرعية للموظف)
-    { path: '/Employee/Leaves', name: 'EmployeeLeaves', component: EmployeeLeaves },
-    {
-  path: '/EmployeeLeaves',
-  name: 'EmployeeLeaves',
-  component: () => import('@/views/employee/EmployeeLeaves.vue')
-}
-
+    { path: '/Employee/Leaves', name: 'EmployeeLeaves', component: EmployeeLeavesView },
   ]
 });
 
